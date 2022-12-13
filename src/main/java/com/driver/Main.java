eca88fd3fc683d29e6d99fd9bf0e0e451676a735
Vaidyanathan Valliappan <vaidyanathan589@gmail.com>
2022-12-13 23:42:47 +0530

    Runtime Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  public static class A{
+      public static String meth()
+      {
+          return "Invoking method from class A";
+      }
+  }
+  public static class B extends A{
+      public static String meth()
+      {
+          return "Method is overridden in Extendend class B";
+      }
+  }
+
+  public static void main(String args[]){
+      B object = new B();
+      B.meth();
+  }
 }
